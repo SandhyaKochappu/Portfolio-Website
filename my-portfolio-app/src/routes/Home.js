b997a531ad2c4fd3ead5b6367b51adcfe3066144
SandhyaKochappu <106261211+SandhyaKochappu@users.noreply.github.com>
2024-04-23 14:48:02 +1000

    Creating react components
    Creating react components for different homepages

--- my-portfolio-app/src/routes/Home.js
+++ my-portfolio-app/src/routes/Home.js
@@ -1,8 +1,12 @@
 import react from 'react'
+import Navbar from '../components/Navbar'
 
 const Home = () => {
     return(
-        <div>Home</div>
+        //calling Navbar.js file from home page
+        <div>                    
+        <Navbar/>
+        </div>
     )
 }
 
