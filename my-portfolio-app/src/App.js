78646aaca841f9167a4ebe5c4ab461c2d249ae25
SandhyaKochappu <106261211+SandhyaKochappu@users.noreply.github.com>
2024-04-23 14:12:57 +1000

    Route setting for different pages
    Setting up React Route for different pages

--- my-portfolio-app/src/App.js
+++ my-portfolio-app/src/App.js
@@ -1,11 +1,23 @@
+//parent page, call other pages from this page
 import React from "react";
 import './index.css';
+//importing pages
+import Home from "./routes/Home";
+import About from "./routes/About";
+import Portfolio from "./routes/Portfolio";
+import Resume from "./routes/Resume";
 
+import { Route, Routes } from "react-router-dom";
 function App() {
   return (
-    <div className="App">
-
-    </div>
+    <>
+      <Routes>
+        <Route path="/" element = {<Home />}/>
+        <Route path="/About" element = {<About />}/>
+        <Route path="/Portfolio" element = {<Portfolio />}/>
+        <Route path="/Resume" element = {<Resume />}/>
+      </Routes>
+    </>
   );
 }
 
---
+++ my-portfolio-app/src/routes/Resume.js
@@ -0,0 +1,10 @@
+//rafce-> to automatically populate the div
+import React from 'react'
+
+const Resume = () => {
+  return (
+    <div>Resume</div>
+  )
+}
+
+export default Resume
