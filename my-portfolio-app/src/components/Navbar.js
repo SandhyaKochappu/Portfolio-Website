8b8251531488848f8a07d61f533ebab4e3156f6e
SandhyaKochappu <106261211+SandhyaKochappu@users.noreply.github.com>
2024-04-24 12:23:25 +1000

    Navbar.js update
    Updated with header background

--- my-portfolio-app/src/components/Navbar.js
+++ my-portfolio-app/src/components/Navbar.js
@@ -24,7 +24,7 @@ const Navbar = () => {
 
   return (
     //Portfolio link pointing to home page
-    <div className="header">
+    <div className={color ? "header header-bg" : "header"}>
     <Link to="/">
     <h1>Portfolio</h1>
     </Link>
