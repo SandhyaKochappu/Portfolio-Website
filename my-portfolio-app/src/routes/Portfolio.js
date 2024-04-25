4442f5ba9e13b20efcea44e9944b45f368cff1c0
SandhyaKochappu <106261211+SandhyaKochappu@users.noreply.github.com>
2024-04-25 21:59:10 +1000

    Update Portfolio.js
    Updating with Github .js file call

--- my-portfolio-app/src/routes/Portfolio.js
+++ my-portfolio-app/src/routes/Portfolio.js
@@ -3,13 +3,15 @@ import React from 'react';
 import Navbar from '../components/Navbar';
 import Footer from '../components/Footer';
 import Heroimageothers from "../components/Heroimageothers";
+import PortfolioGithub from "../components/PortfolioGithub";
 
 const Portfolio = () => {
   return (
     <div>                    
-        <Navbar/>
-        <Footer />
+        <Navbar/>        
         <Heroimageothers heading="Projects." text="Have a look at some of my recent works."/>
+        <PortfolioGithub />
+        <Footer />
     </div>
   )
 }
