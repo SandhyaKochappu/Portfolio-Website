cf38348d1ff6c9614f00bc057f5c7ca4513f03bd
SandhyaKochappu <106261211+SandhyaKochappu@users.noreply.github.com>
2024-04-25 22:48:36 +1000

    Bootstrap installation
    Bootstrap was installed and imported

--- my-portfolio-app/src/components/PortfolioGithub.js
+++ my-portfolio-app/src/components/PortfolioGithub.js
@@ -8,8 +8,7 @@ const PortfolioGithub = () => {
 
   const getProjects = async () => {
       const response = await fetch('https://api.github.com/users/SandhyaKochappu/repos');
-      setProjects(await response.json());
-      
+      setProjects(await response.json());      
     }
 
   useEffect(() => {
@@ -19,21 +18,30 @@ const PortfolioGithub = () => {
   return (
     <div>
       <h5>List of Projects</h5>
-        <div className="container-fluid mt-5">
-          <div className="row text-center">
+        <div class="container-fluid mt-5">
+          <div class="row text-center">
             {
               projects.map((curElem) => {
                 return (
-                  <div className="col-10 col-md-4 mt-5" key={curElem.id}>
-                  <div className="card p-2">
-                  <div className="d-flex align-items-center">
-                  <div className="img"><img src="" className="rounded" width="155" /> </div>
-                  <div className="ml-3 w-100">
-                    <h6 className="mb-0 mt-0 textleft">{curElem.name}</h6>
+                  
+                  <div class="col-10 col-md-4 mt-5" key={curElem.id}>
+                  <div class="card p-2">
+                  <div class="d-flex align-items-center">
+                  <div class="img"><img src="" class="rounded" width="155" /> </div>
+                  <div class="ml-3 w-100">
+                    <h6 class="mb-0 mt-0 textleft">{curElem.name}</h6>
+                    </div>
                    </div>
                    </div>
                    </div>
-                  </div>
+                  
+
+
+
+
+
+
+
                 )
               })
             }           
--- my-portfolio-app/src/routes/Portfolio.js
+++ my-portfolio-app/src/routes/Portfolio.js
@@ -4,14 +4,15 @@ import Navbar from '../components/Navbar';
 import Footer from '../components/Footer';
 import Heroimageothers from "../components/Heroimageothers";
 import PortfolioGithub from "../components/PortfolioGithub";
+import "../../node_modules/bootstrap/dist/css/bootstrap.css";
 
 const Portfolio = () => {
   return (
     <div>                    
-        <Navbar/>        
-        <Heroimageothers heading="Projects." text="Have a look at some of my recent works."/>
+        <Navbar/> 
+        <Heroimageothers heading="Projects." text="Have a look at some of my recent works."/>*/
         <PortfolioGithub />
-        <Footer />
+        <Footer />        
     </div>
   )
 }
