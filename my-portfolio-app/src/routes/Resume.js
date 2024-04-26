b82b3f5090c8bbe9e3aeaefa22468c7c4215ffa4
SandhyaKochappu <106261211+SandhyaKochappu@users.noreply.github.com>
2024-04-26 21:42:35 +1000

    installing ag-grid

--- my-portfolio-app/src/routes/Resume.js
+++ my-portfolio-app/src/routes/Resume.js
@@ -1,16 +1,23 @@
 //rafce-> to automatically populate the div
-import React from 'react'
+import React from 'react';
 import Navbar from '../components/Navbar';
 import Footer from '../components/Footer';
 import Heroimageothers from "../components/Heroimageothers";
 import Card from "../components/Card";
+import Edu from "../assets/Edu.jpg";
+
 
 const Resume = () => {
   return (
     <div>                    
         <Navbar/>
         <Heroimageothers heading="Resume" text="Here's my latest resume."/>
-        <Card />
+        <Card imgSrc={Edu}
+          imgAlt="Card Image"
+          title="Education"
+          buttonText="Learn more"
+          link="cardPage"
+        />
         <Footer />
     </div>
   )
