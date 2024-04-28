2b13b37c869abf2e8a5f077df3cc33922a472819
SandhyaKochappu <106261211+SandhyaKochappu@users.noreply.github.com>
2024-04-28 12:59:06 +1000

    Create Profile.js
    Created Profile page to dispaly GITHUB repos data

---
+++ my-portfolio-app/src/components/Profile.js
@@ -0,0 +1,16 @@
+import React from 'react';
+/*props:receives the ...repos argument passed from Searchbar.js in Profile div */
+
+const Profile = (props) => {
+  return (
+    <>
+        <article className="p-5 bg-white rounded-lg shadow
+        shadow-emerald-300 mt-1">
+         <img src="" alt="" />
+         <h2>Name</h2>   
+        </article>
+    </>
+  )
+}
+
+export default Profile
