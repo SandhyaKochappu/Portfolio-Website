e7099f07824e783ba6741ff2ebeca47c38fada8c
SandhyaKochappu <106261211+SandhyaKochappu@users.noreply.github.com>
2024-04-28 15:48:15 +1000

    Update Profile.js
    Removed headings and text

--- my-portfolio-app/src/components/Profile.js
+++ my-portfolio-app/src/components/Profile.js
@@ -1,16 +1,21 @@
 import React from 'react';
-/*props:receives the ...repos argument passed from Searchbar.js in Profile div */
+/*props:receives the ...repos argument passed from Searchbar.js in Profile div 
+Tailwind’s utility class is used  to style this content.*/
 
-const Profile = (props) => {
+export default function Profile(props) {
+    
   return (
     <>
         <article className="p-5 bg-white rounded-lg shadow
-        shadow-emerald-300 mt-1">
-         <img src="" alt="" />
-         <h2>Name</h2>   
+        shadow-emerald-300 mt-10">
+         <img src={props.owner.avatar_url} alt={props.owner.login}
+         className='w-20 h-20 rounded-full shadow' />
+         <div>
+         <h2>{props.owner.login}</h2> 
+         <p></p>
+         </div>  
         </article>
     </>
-  )
+  )  
 }
 
-export default Profile
