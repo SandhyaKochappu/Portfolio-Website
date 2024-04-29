d97fe57084b15380693501e742b0dcace8c274d5
SandhyaKochappu <106261211+SandhyaKochappu@users.noreply.github.com>
2024-04-29 20:42:02 +1000

    Update SearchBar.js

--- my-portfolio-app/src/components/SearchBar.js
+++ my-portfolio-app/src/components/SearchBar.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import Loading from './Loading';
 import Profile from './Profile';
+//import dotenv from  '../../node_modules/dotenv';
 
 
 /*Using react hooks: useState, useEffect, and asynchronous programming
@@ -9,45 +10,59 @@ import Profile from './Profile';
  ...repos: spread operator to get the rest of the properties in the repos list */ 
 
 const SearchBar = () => {
-    const [searchInput, setSearchInput] = useState("SandhyaKochappu");
+    const [searchInput, setSearchInput] = useState(" ");
     const [repos, setRepos ] = useState([]);
     const [users] = useState("SandhyaKochappu");
     const apiToken = process.env.REACT_APP_API_TOKEN;    
-
+    
     const handleChange = (e) => {
         setSearchInput(e.target.value)
     };       
-       
-  
+    
+    const handleClick = () => {
+        console.log(searchInput);
+
+    };
 
     useEffect(() => {
        const fetchRepos = async () => {
-            const res = await fetch(`https://api.github.com/users/${users}/repos`);
-            const data = await res.json()
-            console.log({apiToken});
+            const res = await fetch(`https://api.github.com/users/${users}/repos`,
+                        {
+                            headers: {
+                                Authorization: `Bearer ${apiToken}`
+                            },
+                        });        
+            const data = await res.json() 
+                     
             setRepos(data);
        }
        fetchRepos()            
   }, []);
 
 return (
-    <>
-            {!repos ? <Loading /> :
+    <> 
+    <div style={{padding: "20px"}}>
+        <input type="text" placeholder='search'
+            value={searchInput} onChange={handleChange}
+        />
+        <button onClick={handleClick}>Search</button>
+        </div>
+
+        {!repos ? <Loading /> :
             (
                 <>
                 <section className='pt-20 pb-20'>                      
                     <div className="grid grid-cols-1 gap-5 md:grid-cols-2 lg:grid-cols-3 mt-10">
                         {repos.map((repos) => (
                             <Profile key={repos.id} {...repos}/>
-            ))}
-                        
+            ))}                       
                     </div> 
                 </section>
                 </>
             ) 
         };
-       </> 
-       
+        
+       </>       
 );
 
 }
