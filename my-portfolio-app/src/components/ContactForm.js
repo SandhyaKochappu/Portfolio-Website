6bc17f006acd9f2504c2df747454e0e32240cc9f
SandhyaKochappu <106261211+SandhyaKochappu@users.noreply.github.com>
2024-04-30 13:06:00 +1000

    Update ContactForm.js
    Updated with contact form components

--- my-portfolio-app/src/components/ContactForm.js
+++ my-portfolio-app/src/components/ContactForm.js
@@ -1,9 +1,42 @@
 import "./ContactFormStyles.css";
-import React from 'react';
+import { useForm, ValidationError } from '@formspree/react';
+/*The useForm hook returns a state object and a handleSubmit function which you pass to the onSubmit form attribute. Combined,
+ these provide a way to submit the form data via AJAX and update
+ form state depending on the response received.*/
 
 const ContactForm = () => {
+
+  const [state, handleSubmit] = useForm(process.env.REACT_APP_FORM_ID);
+
+  if (state.succeeded) {
+    return <p>Thanks for your submission!</p>;
+  }
+
+    
+
   return (
-    <div>ContactForm</div>
+    <div className="form-container">
+    <form method="POST" onSubmit={handleSubmit}>
+            <div className="form-group">
+            <label htmlFor="name">Full Name:</label>
+            <input type="text" name="name" id="name" required />
+            <ValidationError prefix="Name" field="name" errors={state.errors} />
+            </div>
+            <div className="form-group">
+            <label htmlFor="email">Email:</label>
+            <input type="email" name="email" id="email" required />
+            <ValidationError prefix="Email" field="email" errors={state.errors} />
+            </div>
+            <div className="form-group">
+            <label htmlFor="message">Message:</label>
+            <textarea name="message" id="message" required></textarea>
+            <ValidationError prefix="Message" field="message" errors={state.errors} />
+            </div>
+            <button type="submit" disabled={state.submitting}>Send</button>
+            <ValidationError errors={state.errors} />
+        </form>
+        </div>
+    
   )
 }
 
